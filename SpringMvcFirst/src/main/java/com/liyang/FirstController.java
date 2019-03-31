a811bf151787b5c52a357d01bdb2cb4cb02ae9c9
Liyang <370920626@qq.com>
2019-03-31 19:48:56 +0800

    Spring MVC 入门

---
+++ SpringMvcFirst/src/main/java/com/liyang/FirstController.java
@@ -0,0 +1,18 @@
+package com.liyang;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "This is my first Spring mvc application  yyy");
+        modelAndView.setViewName("./index.jsp");
+        return modelAndView;
+    }
+
+}
--- SpringMvcFirst/src/main/webapp/index.jsp
+++ SpringMvcFirst/src/main/webapp/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
-  User: LIYANG
+  User: HUSHUHUA
   Date: 2019/3/31
-  Time: 16:01
+  Time: 16:26
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,6 +11,6 @@
     <title>$Title$</title>
   </head>
   <body>
-  这是第一次修改
+  ${msg} &nbsp; 你妹的哦
   </body>
 </html>
